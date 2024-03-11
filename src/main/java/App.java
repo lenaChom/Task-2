c9eaeee5e3f1f8695d6e99466f9a42685ba13baf
Elena Chumakova <lenap1895@gmail.com>
2024-03-11 12:24:29 +0300

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println("Переменные ссылаются на один и тот же объект? "+(bean==bean2));
+
+
+
+        Cat one = (Cat) applicationContext.getBean("cat");
+        System.out.println(one.getMessage());
+        Cat two = (Cat) applicationContext.getBean("cat");
+        System.out.println(two.getMessage());
+        System.out.println("Переменные ссылаются на один и тот же объект? "+(one==two));
+
     }
 }
